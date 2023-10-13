c36e3263fcc38c17f2e38b4565710b280efca3f2
Tim Hausler <work@Tims-MacBook-Pro.local>
2023-10-13 14:43:20 -0700

    add asdf

--- setup.sh
+++ setup.sh
@@ -6,3 +6,6 @@ SCRIPT_DIR="$(cd "$(dirname $0)" &> /dev/null && pwd)"
 # zsh configuration
 rm -rf ~/.oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# asdf
+brew install asdf
